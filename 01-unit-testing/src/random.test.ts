359fa02741e978d71cf2ddda233431e57abb4fbb
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-18 11:29:30 +0100

    workshop: tests for random number generator

---
+++ 01-unit-testing/src/random.test.ts
@@ -0,0 +1,18 @@
+import { expect, it } from 'vitest';
+import { getRandomNumber } from './random';
+
+it.todo('Generates a random number between 1-10', () => {
+
+});
+
+
+//it.todo -> it is not implemented yet, skipped
+it.todo('Generates a random number between 1-50', () => {
+    
+});
+
+
+
+it.todo('Generates 10 random numbers between 1-20', () => {
+    
+});
---
+++ 01-unit-testing/src/random.ts
@@ -0,0 +1,4 @@
+// random number between 1-10
+export const getRandomNumber = () => {
+    return Math.floor(Math.random() * 10) + 1;
+};
