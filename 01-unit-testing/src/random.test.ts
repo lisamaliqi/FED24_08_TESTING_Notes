d6693f16421c1ed83e1a6d27075e8bfc39fdba35
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-18 13:37:50 +0100

    johans solution for the workshop (better solution)

--- 01-unit-testing/src/random.test.ts
+++ 01-unit-testing/src/random.test.ts
@@ -2,32 +2,32 @@ import { expect, it } from 'vitest';
 import { getRandomNumber } from './random';
 
 it('Generates a random number between 1-10', () => {
-    const randomNumber = getRandomNumber(1, 10, 1);
-    expect(randomNumber).toHaveLength(1);
-    randomNumber.forEach((number) => {
-        expect(number).toBeGreaterThanOrEqual(1);
-        expect(number).toBeLessThanOrEqual(10);
-    });
+    const max = 10;
+    const randomNumber = getRandomNumber(max);
+
+    expect(randomNumber).toBeGreaterThanOrEqual(1);
+    expect(randomNumber).toBeLessThanOrEqual(max);
 });
 
 
 //it.todo -> it is not implemented yet, skipped
 it('Generates a random number between 1-50', () => {
-    const randomNumber = getRandomNumber(1, 50, 1);
-    expect(randomNumber).toHaveLength(1);
-    randomNumber.forEach((number) => {
-        expect(number).toBeGreaterThanOrEqual(1);
-        expect(number).toBeLessThanOrEqual(50);
-    });
+    const max = 10;
+    const randomNumber = getRandomNumber(max);
+
+    expect(randomNumber).toBeGreaterThanOrEqual(1);
+    expect(randomNumber).toBeLessThanOrEqual(max);
 });
 
 
 
 it('Generates 10 random numbers between 1-20', () => {
-    const randomNumber = getRandomNumber(1, 20, 10);
-    expect(randomNumber).toHaveLength(10);
-    randomNumber.forEach((number) => {
-        expect(number).toBeGreaterThanOrEqual(1);
-        expect(number).toBeLessThanOrEqual(20);
-    });
+    const max = 10;
+    
+    for(let i = 0; i < max; i++) { //loop to create 10 random numbers
+        const randomNumber = getRandomNumber(max);// generate the number 10 times
+
+        expect(randomNumber).toBeGreaterThanOrEqual(1);
+        expect(randomNumber).toBeLessThanOrEqual(max);
+    }
 });
--- 01-unit-testing/src/random.ts
+++ 01-unit-testing/src/random.ts
@@ -1,4 +1,10 @@
 // random number between 1-10
-export const getRandomNumber = (min: number, max: number, arrayLength: number) => {
-    return Array.from({ length: arrayLength }, () => Math.floor(Math.random() * max) + min);
+/* export const getRandomNumber = ( max = 10, arrayLength: number) => { //default value for max is 10
+    return Array.from({ length: arrayLength }, () => Math.floor(Math.random() * max) + 1);
+}; */
+
+
+//johans example
+export const getRandomNumber = ( max = 10) => { //default value for max is 10
+    return Math.floor(Math.random() * max) + 1;
 };
