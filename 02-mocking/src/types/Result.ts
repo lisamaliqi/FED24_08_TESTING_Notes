688aa735e53cbf74be05460e825a468923ce4525
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-20 09:25:21 +0100

    adding example of types

--- 02-mocking/src/types/Result.ts
+++ 02-mocking/src/types/Result.ts
@@ -2,3 +2,12 @@ export type Result = {
 	success: boolean;
 	error?: string;
 };
+
+
+/* 
+export type Result = { you can also do this 
+    success: true; if its true, it does not contain error
+} | {
+	success: false; if its false, it contains error
+	error: string;
+}; */
