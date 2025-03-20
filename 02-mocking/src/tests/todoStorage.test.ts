d7b1a431d3cc02c39d36d2ae16ec53aee0b72cc5
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-20 09:30:12 +0100

    test skeleton for todo storage

---
+++ 02-mocking/src/tests/todoStorage.test.ts
@@ -0,0 +1,23 @@
+import { describe, expect, it } from 'vitest';
+import { getTodos, saveTodos } from '../utils/todoStorage';
+
+describe('Get todos', () => {
+    it.todo('returns empty list of todos', () => {
+
+    });
+
+
+});
+
+
+describe('Save todos', () => {
+    it.todo('can save a todo', () => {
+
+    });
+
+    it.todo('can save a todo and then retrieve it', () => {
+
+    });
+
+    
+});
