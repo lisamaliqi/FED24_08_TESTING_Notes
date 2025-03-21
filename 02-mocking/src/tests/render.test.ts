ac5c53f4e2fcec24d095e5ccf134152719e43631
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-21 14:29:16 +0100

    Test that we can render a single todo. Also added a smol workshop

--- 02-mocking/src/tests/render.test.ts
+++ 02-mocking/src/tests/render.test.ts
@@ -1,13 +1,35 @@
+/**
+  * @vitest-environment happy-dom
+  */
 import { describe, expect, it } from "vitest";
- import { transformTodosToHtml } from "../utils/render";
+import { transformTodosToHtml } from "../utils/render";
+import testTodos from "./data/todos";
  
- describe("renders todos", () => {
+describe("renders todos", () => {
  
- 	it("should output empty list when no todos exist", () => {
+    it("should output empty list when no todos exist", () => {
  		const lis = transformTodosToHtml([]);
  		expect(lis).toBe("");
  	});
  
- 	it.todo("should output list with one todo", () => {});
+
+ 	it("should output list with one todo", () => {
+        const rawHTML = transformTodosToHtml([ testTodos[0] ]); //take the first todo from the todos file in data folder
+        document.body.innerHTML = `<ul>${rawHTML}</ul>`; //put it in a ul element
+
+        // query for any listitems with the `todo` class
+        const liEls = document.querySelectorAll("li.todo");
+        expect(liEls).toHaveLength(1);
+    });
+
+
+    //workshop!!
+    it.todo("should output list with many todo", () => {});
+
+
+    it.todo("should output list with one completed todo", () => {});
+
+
+    it.todo("should output list with one todo that isn't completed", () => {});
  
- });
+});
