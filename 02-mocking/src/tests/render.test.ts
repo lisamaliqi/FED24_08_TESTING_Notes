9a022ac6e9c881000ea4ee6306dd19049a5ae3a3
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-24 10:59:55 +0100

    finishing workshop

--- 02-mocking/src/tests/render.test.ts
+++ 02-mocking/src/tests/render.test.ts
@@ -4,6 +4,19 @@
 import { describe, expect, it } from "vitest";
 import { transformTodosToHtml } from "../utils/render";
 import testTodos from "./data/todos";
+
+// find the first finished todos
+const finishedTodo = testTodos.find(todo => todo.completed) ?? {
+    ...testTodos[0],
+    completed: true,
+};
+
+// find the first unfinished todos
+const unfinishedTodo = testTodos.find(todo => !todo.completed) ?? {
+    ...testTodos[0],
+    completed: false,
+};
+
  
 describe("renders todos", () => {
  
@@ -24,12 +37,32 @@ describe("renders todos", () => {
 
 
     //workshop!!
-    it.todo("should output list with many todo", () => {});
+    it("should output list with many todos", () => {
+        const rawHTML = transformTodosToHtml(testTodos);
+        document.body.innerHTML = `<ul>${rawHTML}</ul>`;
+
+        const liEls = document.querySelectorAll("li.todo");
+        expect(liEls).toHaveLength(testTodos.length);
+    });
 
 
-    it.todo("should output list with one completed todo", () => {});
+    it("should output list with one completed todo", () => {
+        const rawHTML = transformTodosToHtml([ finishedTodo ]);
+        document.body.innerHTML = `<ul>${rawHTML}</ul>`;
+
+        // query for a listitem with the `todo` and the `completed` class
+ 		const liEls = document.querySelectorAll("li.todo.completed");
+ 		expect(liEls).toHaveLength(1);
+ 	});
 
 
-    it.todo("should output list with one todo that isn't completed", () => {});
+     it("should output list with one todo that isn't completed", () => {
+        const rawHTML = transformTodosToHtml([ unfinishedTodo ]);
+        document.body.innerHTML = `<ul>${rawHTML}</ul>`;
+
+        // query for a listitem with the `todo` class and NOT the `completed` class
+        const liEls = document.querySelectorAll("li.todo:not(.completed)");
+        expect(liEls).toHaveLength(1);
+    });
  
 });
