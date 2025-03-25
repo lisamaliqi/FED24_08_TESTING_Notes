9a1f1fa3fdb0c46e896c1c96798b25281d09347a
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-25 09:20:10 +0100

    installing boilerplate and doing npm install on 03-integration testing

---
+++ 03-integration-testing/frontend/src/types/Result.ts
@@ -0,0 +1,12 @@
+export type Result<T = void> = ResultSuccess<T> | ResultError;
+
+export type ResultSuccess<T> = {
+	success: true;
+	data: T;
+};
+
+export type ResultError = {
+	success: false;
+	error: string;
+	exception?: any;
+};
