5dc142303f1b0e86ac2a8eef080a4baf0b5e2612
lisamaliqi <lisamaliqi@hotmail.com>
2025-03-25 13:07:01 +0100

    doing my solution to the workshop

--- 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
+++ 03-integration-testing/frontend/src/tests/TodoAPI.test.ts
@@ -17,7 +17,7 @@ describe('TodoAPI', () => {
 	it("should create a todo", async () => {
         const todos = await TodoAPI.createTodo(newTodo);
 
-        console.log(todos)
+        // console.log(todos)
         expect(todos.id).toBeTypeOf('number');
         expect(todos.title).toBe(newTodo.title);
         expect(todos.completed).toBe(newTodo.completed);
@@ -30,11 +30,71 @@ describe('TodoAPI', () => {
         });
     });
 
-	it.todo("should create and then get the todo", () => {});
 
-	it.todo("should create and then find the todo among all todos", () => {});
+    //Workshop!!
+	it("should create and then get the todo", async () => {
+        const todos = await TodoAPI.createTodo(newTodo);
+        const get = await TodoAPI.getTodo(todos.id);
 
-	it.todo("should create and then update the todo", () => {});
+        // console.log('todos: ', todos);
+        // console.log('get: ', get);
+        expect(todos).toMatchObject({
+            id: expect.any(Number),
+            title: newTodo.title,
+            completed: newTodo.completed,
+        });
+        expect(get).toMatchObject({
+            id: todos.id,
+            title: todos.title,
+            completed: todos.completed,
+        });
+    });
 
-	it.todo("should create and then delete the todo", () => {});
+	it("should create and then find the todo among all todos", async () => {
+        const todo = await TodoAPI.createTodo(newTodo);
+        const getAll = await TodoAPI.getTodos();
+
+        // console.log('todos: ', todo);
+        // console.log('GetAll: ', getAll);
+
+        expect(todo).toMatchObject({
+            id: expect.any(Number),
+            title: newTodo.title,
+            completed: newTodo.completed,
+        });
+        expect(getAll).toContainEqual(todo);
+    });
+
+	it("should create and then update the todo", async () => {
+        const todo = await TodoAPI.createTodo(newTodo);
+        const update = await TodoAPI.updateTodo(todo.id, todo);
+
+        // console.log('todos: ', todo);
+        // console.log('update: ', update);
+        expect(todo).toMatchObject({
+            id: expect.any(Number),
+            title: newTodo.title,
+            completed: newTodo.completed,
+        });
+        expect(update).toMatchObject({
+            id: todo.id,
+            title: todo.title,
+            completed: todo.completed,
+        });
+    });
+
+	it("should create and then delete the todo", async () => {
+        const todo = await TodoAPI.createTodo(newTodo);
+        const deleteTodo = await TodoAPI.deleteTodo(todo.id);
+
+        console.log('todos: ', todo);
+        console.log('deleteTodo: ', deleteTodo);
+        expect(todo).toMatchObject({
+            id: expect.any(Number),
+            title: newTodo.title,
+            completed: newTodo.completed,
+        });
+        expect(deleteTodo).toMatchObject({});
+
+    });
 });
