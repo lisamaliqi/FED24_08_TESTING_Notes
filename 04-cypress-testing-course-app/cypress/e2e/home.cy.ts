53fd954ff5c44db8251402a9e6d468be31f0e95a
lisamaliqi <lisamaliqi@hotmail.com>
2025-04-01 16:24:10 +0200

    Test that the features on the homepage are correct.

--- 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
+++ 04-cypress-testing-course-app/cypress/e2e/home.cy.ts
@@ -1,10 +1,31 @@
-describe("template spec", () => {
-  it("the h1 contains the correct text", () => {
-    cy.visit("http://localhost:3000");
+describe("Home Page", () => {
+	beforeEach(() => {
+		// Visit page
+		cy.visit("http://localhost:3000");
 
-    // Wait for the app to be fully hydrated and visible 💦
-    cy.get("#__next").should("be.visible");
+		// Wait for the app to be fully hydrated and visible 💦
+		cy.get("#__next").should("be.visible");
+	});
 
-    cy.get("h1").contains("Testing Next.js Applications with Cypress");
-  });
+
+
+	it("the h1 contains the correct text", () => {
+		// Find the h1 and verify content
+		// cy.get('[data-test="hero-heading"]')  // 🫤
+		cy.get("h1")
+			.contains("Testing Next.js Applications with Cypress");
+	});
+
+  
+
+	it("the features on the homepage are correct", () => {
+		// Find the first definition term and verify content
+		cy.get("dt").eq(0).contains("4 Courses");
+
+		// Find the second definition term and verify content
+		cy.get("dt").eq(1).contains("25+ Lessons");
+
+		// Find the third definition term and verify content
+		cy.get("dt").eq(2).contains("Free and Open Source");
+	});
 });
